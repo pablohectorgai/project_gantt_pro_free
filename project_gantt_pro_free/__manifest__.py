cfbc7f97c494457b59d72b1a8269891548b613e8
pablohectorgai <pablohectorgonzalezjr@gmail.com>
2025-10-31 17:53:45 -0600

    Simplify gantt client action assets

--- project_gantt_pro_free/__manifest__.py
+++ project_gantt_pro_free/__manifest__.py
@@ -4,20 +4,14 @@
     "version": "17.0.1.0.0",
     "license": "LGPL-3",
     "author": "MetaProject",
-    "depends": ["web", "project"],
+    "depends": ["base", "web", "project"],
     "assets": {
         "web.assets_backend": [
-            "project_gantt_pro_free/static/lib/frappe-gantt.min.css",
-            "project_gantt_pro_free/static/lib/frappe-gantt.min.js",
-            "project_gantt_pro_free/static/src/xml/gantt_free_templates.xml",
-            "project_gantt_pro_free/static/src/js/gantt_free_model.js",
-            "project_gantt_pro_free/static/src/js/gantt_free_renderer.js",
-            "project_gantt_pro_free/static/src/js/gantt_free_controller.js",
-            "project_gantt_pro_free/static/src/js/gantt_free_view.js",
+            "project_gantt_pro_free/static/src/js/gantt_client_action.js",
+            "project_gantt_pro_free/static/src/xml/gantt_templates.xml",
         ],
     },
     "data": [
-        "views/assets.xml",
         "views/project_task_gantt_view.xml",
     ],
     "application": False,
